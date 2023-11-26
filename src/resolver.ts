0c8b77ae06e4d9439c1367d61e255b57bd7b3497
interrrp <int@addictedto.coffee>
2023-11-26 13:14:45 +0800

    Simplify script path resolver

--- src/resolver.ts
+++ src/resolver.ts
@@ -3,8 +3,6 @@ import { join as joinPaths } from "std/path/mod.ts";
 export function resolveScriptPathForURL(url: string): string {
   return joinPaths(
     "cgi-bin",
-    url
-      .split("/").slice(3).join("/") // Remove protocol and domain
-      .replace(/\?.+/, ""), // Remove query params
+    new URL(url).pathname,
   );
 }
